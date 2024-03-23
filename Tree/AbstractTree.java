37f68e730c9f330b886b980f5b4a1dcf4864f123
Arthur Chien <moneychien20639@gmail.com>
2024-03-23 12:46:25 +0800

    add postorer and preorder traversal funcs

--- Tree/AbstractTree.java
+++ Tree/AbstractTree.java
@@ -1,6 +1,12 @@
 package Tree;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import Lists.Position;
+import Queues.LinkedQueue;
+import Queues.Queue;
 
 public abstract class AbstractTree<E> implements Tree<E> {
     public boolean isInternal(Position<E> p){return numChildren(p) > 0;}
@@ -24,4 +30,71 @@ public abstract class AbstractTree<E> implements Tree<E> {
         }
         return h;
     }
+
+    private void preorderSubtree(Position<E> p, List<Position<E>> snapshot){
+        snapshot.add(p);
+        for (Position<E> c : children(p)){
+            preorderSubtree(c, snapshot);
+        }
+    }
+
+    public Iterable<Position<E>> preorder(){
+        List<Position<E>> snapshot = new ArrayList<>();
+        if(!isEmpty())
+            preorderSubtree(root(), snapshot);
+        return snapshot;
+    }
+
+    private void postorderSubtree(Position<E> p, List<Position<E>> snapshot){
+        for (Position<E> c : children(p)){
+            postorderSubtree(c, snapshot);
+        }
+        snapshot.add(p);
+    }
+
+    public Iterable<Position<E>> postorder(){
+        List<Position<E>> snapshot = new ArrayList<>();
+        if(!isEmpty())
+            postorderSubtree(root(), snapshot);
+        return snapshot;
+    }
+
+    public Iterable<Position<E>> breadfirst(){
+        List<Position<E>> snapshot = new ArrayList<>();
+        if (!isEmpty()){
+            Queue<Position<E>> fringe = new LinkedQueue<>();
+            fringe.enqueue(root());
+            while (!fringe.isEmpty()) {
+                Position<E> p = fringe.dequeue();
+                snapshot.add(p);
+                for (Position<E> c : children(p)){
+                    fringe.enqueue(c);
+                }
+            }
+        }
+        return snapshot;
+    }
+
+    private class ElemetIterator implements Iterator<E> {
+        Iterator<Position<E>> posIterator = positions().iterator();
+
+        public boolean hasNext() {
+            return posIterator.hasNext();
+        }
+        public E next() {
+            return posIterator.next().getElement();
+        }
+        public void remove() {
+            posIterator.remove();
+        }
+    }
+
+    public Iterator<E> iterator() {
+        return new ElemetIterator();
+    }
+
+    public Iterable<Position<E>> positions() {
+        return preorder();
+    }
+
 }
